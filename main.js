5e4355adc53a235d3b4a53f97e42adb9594c709d
r-mcgrath <97359037+r-mcgrath@users.noreply.github.com>
2023-09-13 21:55:42 -0400

    commit

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+window.addEventListener("DOMContentLoaded", (e) => {
+    let catImgURL = 'wideEyedCat.jpeg'
+document.body.style.backgroundImage = `url(${catImgURL})`;
+
+
+function getCatImg() {
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then((response) => response.json())
+      .then((json) => {
+        catImgURL = json[0].url;
+        document.body.style.backgroundImage = `url(${catImgURL})`;
+      })
+      .catch((error) => {
+        console.error('Error fetching cat image:', error);
+      });
+  }
+  
+  const button = document.querySelector('button');
+  button.addEventListener('click', getCatImg);
+})
 
